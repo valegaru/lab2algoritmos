d91fe34eba561f92107d29fa270de17d5f384f61
valegaru <valegaru2003@gmail.com>
2024-02-22 13:23:00 -0600

    norenderiza

--- public/src/index.js
+++ public/src/index.js
@@ -1,1 +1,24 @@
 //abuelo
+// importar todo de padre
+
+import * as components from './components/index';
+
+class AppContainer extends HTMLElement {
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+	}
+
+	connectedCallback() {
+		this.render();
+	}
+
+	render() {
+		this.shadowRoot.innerHTML = `
+			<card-destacados>  hola </card-destacados>
+			`;
+	}
+}
+
+customElements.define('app-container', AppContainer);
+export default AppContainer;
